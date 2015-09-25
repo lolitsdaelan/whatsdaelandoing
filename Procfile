fac3d6364f937f262f40530b28027f42c6409a73
daelan <daelan.scroggins@gmail.com>
2015-09-25 09:55:00 -0400

    Additional files and changes for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn whatsdaelandoing.wsgi
--- whatsdaelandoing/settings/production.py
+++ whatsdaelandoing/settings/production.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 from .base import *
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
